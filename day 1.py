3d953e2bff798143637ce2731a869015bf897ad4
YOOSUF ISMAIL <kiyas9148@gmail.com>
2025-11-16 11:47:39 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,5 @@
+print("Wellcome To The Bend Name Generaror")
+city = input("Which City You Grow Up in ?")
+pet = input("What IS The Name of a Pet?") 
+print("Your bend name could be:" + city + " gg"
+" " + pet)
